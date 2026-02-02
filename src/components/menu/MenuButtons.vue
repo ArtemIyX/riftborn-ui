c4fef318eb4e379e81b68d196974ad0daa04f688
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-02 21:03:59 +0200

    Main menu buttons

---
+++ src/components/menu/MenuButtons.vue
@@ -0,0 +1,25 @@
+﻿<template>
+  <GBox class="menu-buttons">
+    <GStack>
+      <slot/>
+    </GStack>
+  </GBox>
+
+</template>
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+.menu-buttons {
+  /* Container sizing */
+  width: clamp(200px, 25vw, 400px);
+  min-height: min-content;
+  max-height: 80vh;
+
+  /* Spacing */
+  padding: clamp(1rem, 2vw, 2rem);
+}
+</style>
--- src/views/MainMenu/MainMenu.vue
+++ src/views/MainMenu/MainMenu.vue
@@ -1,8 +1,32 @@
 ﻿<template>
-Hello world!
+
+  <MenuButtons class="menu-box">
+    <GButton>
+      <GLocText :table="locTable" key="#Play">Play</GLocText>
+    </GButton>
+    <GButton>
+      <GLocText :table="locTable" key="#FindServers">Find servers</GLocText>
+    </GButton>
+    <GButton>
+      <GLocText :table="locTable" key="#Settings">Settings</GLocText>
+    </GButton>
+    <GButton>
+      <GLocText :table="locTable" key="#Credits">Credits</GLocText>
+    </GButton>
+    <GButton>
+      <GLocText :table="locTable" key="#Social">Social</GLocText>
+    </GButton>
+    <GButton>
+      <GLocText :table="locTable" key="#Quit">Quit</GLocText>
+    </GButton>
+  </MenuButtons>
 </template>
 
 <script setup>
+
+import MenuButtons from "@/components/menu/MenuButtons.vue";
+
+const locTable = 'ST_Menu';
 
 </script>
 
