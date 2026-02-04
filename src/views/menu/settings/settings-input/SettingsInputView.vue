38eb7f8d335e42e6f254e4359dd4790ffed15771
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-02-04 10:26:53 +0200

    Settings view: implementation

--- src/views/menu/settings/settings-input/SettingsInputView.vue
+++ src/views/menu/settings/settings-input/SettingsInputView.vue
@@ -7,28 +7,44 @@
     </GLocText>
   </GHeading>
 
+  <!-- Flex container -->
   <div class="input-settings-container">
+
+    <!-- Occupies free space + scroll bar -->
     <div class="settings-main">
+
+      <!-- For each category -->
       <div v-for="category in inputStore.inputs" :key="category.name">
+
+        <!-- Category title -->
         <GDivider>
           <GLocText :key="category.key" :table="category.table">
             {{ category.name }}
           </GLocText>
         </GDivider>
+
+        <!-- Category input items -->
         <div class="input-grid">
-          <div class="setting-row" v-for="inputData in category.keyBindings">
+          <div class="setting-row" v-for="inputData in category.keyBindings" :key="inputData.name">
             <GLabel>
               <GLocText :key="inputData.key" :table="category.table">
                 {{ inputData.name }}
               </GLocText>
             </GLabel>
-            <GKey>
-
+            <GKey
+              :ref="(el) => setKeyRef(category.name, inputData.name, el)"
+              v-model="inputData.currentKey"
+              @change-start="handleChangeStart(category.name, inputData.name)"
+              @change-complete="(newValue) => handleChangeComplete(category.name, inputData.name, newValue)"
+              @change-cancel="handleChangeCancel"
+            >
             </GKey>
           </div>
         </div>
       </div>
     </div>
+
+    <!-- Always on bottom of page -->
     <div class="settings-footer">
       <GButtonCommon @click="resetToDefaults" style="width: 100%">
         <GLocText key="#ResetToDefaults" table="ST_Menu">
@@ -46,5 +62,60 @@ import {useInputStore} from "@/stores/inputStore.js";
 
 const inputStore = useInputStore();
 
+// Track which key is currently being listened to
+const activeListeningKey = ref(null);
+
+// Store refs to all GKey components
+const keyRefs = ref({});
+
+const setKeyRef = (categoryName, keyName, el) => {
+  if (el) {
+    const refKey = `${categoryName}_${keyName}`;
+    keyRefs.value[refKey] = el;
+  }
+};
+
+const handleChangeStart = (categoryName, keyName) => {
+  const currentRefKey = `${categoryName}_${keyName}`;
+
+  // If there's already an active key being listened to and it's not this one
+  if (activeListeningKey.value && activeListeningKey.value !== currentRefKey) {
+    // Cancel the previously listening key
+    const previousKey = keyRefs.value[activeListeningKey.value];
+    if (previousKey && previousKey.cancel) {
+      previousKey.cancel();
+    }
+  }
+
+  // Set this key as the active listening key
+  activeListeningKey.value = currentRefKey;
+};
+
+const handleChangeComplete = (categoryName, keyName, newValue) => {
+  // Update the store with the new key value
+  inputStore.setCurrentKey(categoryName, keyName, newValue);
+
+  // Clear the active listening key
+  activeListeningKey.value = null;
+};
+
+const handleChangeCancel = () => {
+  // Clear the active listening key
+  activeListeningKey.value = null;
+};
+
+const resetToDefaults = () => {
+  // Cancel any active listening
+  if (activeListeningKey.value) {
+    const activeKey = keyRefs.value[activeListeningKey.value];
+    if (activeKey && activeKey.cancel) {
+      activeKey.cancel();
+    }
+    activeListeningKey.value = null;
+  }
+
+  // Reset all keys to defaults
+  inputStore.resetAllKeys();
+};
 
 </script>
